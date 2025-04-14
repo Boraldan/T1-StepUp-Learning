100367b1f44c8476518243a8a6fbbdb2b3c5cfe3
Boraldan <boraldan@gmail.com>
2025-04-14 14:34:49 +0300

    hw3 starters logaspect + kafka : v_0.2

--- task-aop/src/main/java/ru/boraldan/aop/taskaop/TaskAopApplication.java
+++ task-aop/src/main/java/ru/boraldan/aop/taskaop/TaskAopApplication.java
@@ -3,11 +3,12 @@ package ru.boraldan.aop.taskaop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
 @EntityScan(basePackages = "task.entity")
 @EnableJpaRepositories(basePackages = "ru.boraldan.aop.taskaop.repository")
 @Import({ru.boraldan.taskstarters.logaopstarter.config.LoggerAopAutoConfiguration.class,
