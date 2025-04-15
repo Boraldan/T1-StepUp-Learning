c0bdd8ec0866d41ce26158dde3a2b77f8a0a0fc8
Boraldan <boraldan@gmail.com>
2025-04-15 23:04:15 +0300

    hw3 task/log-starter v_2

--- task-aop/src/main/java/ru/boraldan/aop/taskaop/TaskAopApplication.java
+++ task-aop/src/main/java/ru/boraldan/aop/taskaop/TaskAopApplication.java
@@ -1,12 +1,11 @@
 package ru.boraldan.aop.taskaop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(ru.boraldan.logaopstarter.starter.config.LoggerAopAutoConfiguration.class)
+@ImportAutoConfiguration(ru.boraldan.logaopstarter.starter.config.LoggerAopAutoConfiguration.class)
 public class TaskAopApplication {
 
     public static void main(String[] args) {
